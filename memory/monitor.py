3813f4d11cbbdd63b6bdc2d5d08243284895eba7
Enrique Saurez <esaurez@gatech.edu>
2020-01-20 19:33:24 +0000

    Memory monitor update

--- memory/monitor.py
+++ memory/monitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from __future__ import print_function
+import argparse
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from vm import VMManager
@@ -8,18 +9,29 @@ import sched, time
 
 VM_PREFIX="aos"
 
-def run(sc,vmobjlist,memoryinfolist):
+def run(sc,vmobjlist,machineParseable):
     i = 0
     for vm in vmobjlist:
-        memoryinfolist[i] = vm.memoryStats()
-        print("memory,{},{},{}"
-            .format(vm.name(), 
-                    memoryinfolist[i]['actual'] / 1024.0,
-                    memoryinfolist[i]['unused'] / 1024.0))
+        stats = vm.memoryStats()
+        if machineParseable:
+            print("memory,{},{},{}"
+                    .format(vm.name(), 
+                        stats['actual'] / 1024.0,
+                        stats['unused'] / 1024.0))
+        else:
+            print("Memory (VM: {})  Actual [{}], Unused: [{}]"
+                    .format(vm.name(), 
+                        stats['actual'] / 1024.0,
+                        stats['unused'] / 1024.0))
+
         i+=1
-    sc.enter(2, 1, run, (sc,vmobjlist,memoryinfolist))
+    sc.enter(2, 1, run, (sc,vmobjlist,machineParseable))
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m","--machine",action="store_true",help="outputs a machine parseable format")
+    args = parser.parse_args()
+    machineParseable = args.machine
     s = sched.scheduler(time.time, time.sleep)
     manager = VMManager()
     vmlist = manager.getRunningVMNames(VM_PREFIX)
@@ -28,7 +40,5 @@ if __name__ == '__main__':
     for vm in vmobjlist:
         vm.setMemoryStatsPeriod(1)   
  
-    memoryinfolist = [None] * len(vmobjlist)
-
-    s.enter(2, 1, run, (s,vmobjlist,memoryinfolist))
+    s.enter(2, 1, run, (s,vmobjlist,machineParseable,))
     s.run()
