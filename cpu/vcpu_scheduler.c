39947e1f965617d5108bc504d23fd9317c8c5163
Daniel Vaseekaran <dvaseekara3@gmail.com>
2020-09-12 12:38:43 -0400

    Calculate VCPU Usage Percentage

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -24,6 +24,9 @@ void connectToVM(){
         printf("Failed to get all domains\n");
         return;
     }
+
+    int vcpuTimeArray[8];
+    int vcpuTotalTime;
     for (i = 0; i < ret; i++) {
         virDomainInfo virDomainInfo;
         virDomainGetInfo(domains[i], &virDomainInfo);
@@ -32,29 +35,41 @@ void connectToVM(){
         printf("Domain memory: %ld\n", virDomainInfo.memory);
         printf("Domain nrVirtCpu: %d\n", virDomainInfo.nrVirtCpu);
         printf("Domain cpuTime: %lld\n", virDomainInfo.cpuTime);
+
+        vcpuTimeArray[i] = virDomainInfo.cpuTime/1000000;
+        vcpuTotalTime = vcpuTotalTime + virDomainInfo.cpuTime/1000000;
     }
 
-    for (i = 0; i < ret; i++) {
-        int ncpumaps = 1;
-        int maplen = 3;
-        unsigned char * cpumaps = calloc(ncpumaps, maplen);
-        int vcpuPinInfo_ret;
-        vcpuPinInfo_ret = virDomainGetVcpuPinInfo(domains[i], ncpumaps, cpumaps, maplen, VIR_DOMAIN_AFFECT_CURRENT);
-        if(vcpuPinInfo_ret<0){
-            printf("Failed to get Vcpu Pin info\n");
-            return;
-        }
-
-        int j;
-        printf("Number of virtual cpus %d\n", vcpuPinInfo_ret);
-        for ( j=0; j < maplen ; j++){
-                printf("\tValue of bit %i is %u\n", j, cpumaps[1][j]);
-        }
-        free(cpumaps);
-
-
-        // int	virDomainPinVcpu(virDomainPtr domain, unsigned int vcpu, unsigned char * cpumap, int maplen)
+    float vcpuUsagePercent[8];
+    for (i = 0; i< ret; i++){
+        vcpuUsagePercent[i] = (float) vcpuTimeArray[i]/vcpuTotalTime;
+        printf("VCPU percentage for vcpu%li is %f\n%%", i, vcpuUsagePercent[i]);
     }
+
+
+
+    // for (i = 0; i < ret; i++) {
+    //     int ncpumaps = 1;
+    //     int maplen = 3;
+    //     unsigned char * cpumaps = calloc(ncpumaps, maplen);
+    //     int vcpuPinInfo_ret;
+    //     vcpuPinInfo_ret = virDomainGetVcpuPinInfo(domains[i], ncpumaps, cpumaps, maplen, VIR_DOMAIN_AFFECT_CURRENT);
+    //     if(vcpuPinInfo_ret<0){
+    //         printf("Failed to get Vcpu Pin info\n");
+    //         return;
+    //     }
+
+    //     int j;
+    //     printf("Number of virtual cpus %d\n", vcpuPinInfo_ret);
+    //     for ( j=0; j < maplen ; j++){
+    //             printf("\tValue of bit %i is %u\n", j, cpumaps[1]);
+    //     }
+    //     free(cpumaps);
+
+
+
+    //     // int	virDomainPinVcpu(virDomainPtr domain, unsigned int vcpu, unsigned char * cpumap, int maplen)
+    // }
 
     for (i = 0; i < ret; i++) {
         virDomainFree(domains[i]);
